14ba389dc4d86c329b6b44c9920cde6f50de8f24
Toure2019 <51404116+Toure2019@users.noreply.github.com>
2019-10-02 11:40:27 +0200

    add controller usersCtrl.js and add users route

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var http    = require('http');
+var usersCtrl = require('./usersCtrl');
 
 var app     = express();
 
@@ -12,6 +13,9 @@ app.get('/users', (req, res) => {
 	res.writeHead(200, { 'Content-Type': 'text/html' });
 	res.end('<h1>Utilisateurs</h1>');
 });
+
+app.post('/users/signout', usersCtrl.signout);
+
 
 http.createServer(app).listen(3000, () => {
 	console.log('Express sever listening on port 3000');
